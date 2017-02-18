e1fe340dc6ef9d9b801eb033af3bbd50c6318c6f
correa9543 <correa9543@gmail.com>
2017-02-18 08:53:44 -0800

    Create Stack.h

---
+++ Stack.h
@@ -0,0 +1,41 @@
+#ifndef Stack_h
+#define Stack_h
+
+#include <stdio.h>
+
+
+#ifndef STACK_H
+#define STACK_H
+#include <iostream>
+#include "SLinkedList.h"
+#include "HeaderNode.h"
+using namespace std;
+
+template <class T>
+class Stack : protected SLinkedList<T> {
+public:
+    
+    void push(T& data);
+    void pop();
+    void top();
+};
+
+template <class T>
+void Stack<T>::push(T& data) {
+    this->addFront(data);
+}
+
+template <class T>
+void Stack<T>::pop() {
+    this->removeFront();
+}
+
+template <class T>
+void Stack<T>::top() {
+    if (this->list->count > 0) {
+        cout << *this->list->front->data;
+    }
+}
+#endif STACK_H
+
+#endif /* Stack_hpp */
