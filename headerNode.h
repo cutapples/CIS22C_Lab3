2478eb89740febe4bb2f94bee763d66e20e887fa
correa9543 <correa9543@gmail.com>
2017-02-18 08:56:32 -0800

    Create headerNode.h

---
+++ headerNode.h
@@ -0,0 +1,29 @@
+#ifndef HeaderNode_h
+#define HeaderNode_h
+
+#include <stdio.h>
+
+#ifndef HEADERNODE_H
+#define HEADERNODE_H
+
+template <class T>
+class HeaderNode {
+public:
+    Node<T>* front;
+    Node<T>* back;
+    int count;
+    
+    HeaderNode();
+};
+
+template <class T>
+HeaderNode<T>::HeaderNode() {
+    this->front = nullptr;
+    this->back = nullptr;
+    this->count = 0;
+}
+
+#endif HEADERNODE_H
+
+
+#endif /* HeaderNode_hpp */
