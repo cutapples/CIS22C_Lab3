69989f60b492fd4f5e06c17a623e967f82ea78a3
James May <may.james.m@gmail.com>
2017-02-19 17:52:27 -0800

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,9 +1,3 @@
-#ifndef Stack_h
-#define Stack_h
-
-#include <stdio.h>
-
-
 #ifndef STACK_H
 #define STACK_H
 #include <iostream>
@@ -14,28 +8,43 @@ using namespace std;
 template <class T>
 class Stack : protected SLinkedList<T> {
 public:
-    
-    void push(T& data);
-    void pop();
-    void top();
+
+	void push(T data);
+	void pop(T& data);
+	void pop();
+	void top(T& data);
+	bool isEmpty();
+	void emptyStack();
 };
 
 template <class T>
-void Stack<T>::push(T& data) {
-    this->addFront(data);
+void Stack<T>::push(T data) {
+	this->addFront(data);
+}
+
+template <class T>
+void Stack<T>::pop(T& data) {
+	data = this->list->front->data;
+	this->removeFront();
 }
 
 template <class T>
 void Stack<T>::pop() {
-    this->removeFront();
+	this->removeFront();
 }
 
 template <class T>
-void Stack<T>::top() {
-    if (this->list->count > 0) {
-        cout << *this->list->front->data;
-    }
+void Stack<T>::top(T& data) {
+	data = this->list->front->data;
+}
+
+template <class T>
+bool Stack<T>::isEmpty() {
+	return (this->list->count == 0);
+}
+
+template<class T>
+void Stack<T>::emptyStack() {
+	this->emptyList();
 }
 #endif STACK_H
-
-#endif /* Stack_hpp */
